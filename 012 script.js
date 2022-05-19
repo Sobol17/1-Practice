3e16816346693ad6a008e4a4dff08f894e8e2cad
igor <sobolinskii685@gmail.com>
2022-05-19 14:57:33 +0800

    second tast done

--- 012 script.js
+++ 012 script.js
@@ -27,7 +27,15 @@
 
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
 
+console.log(numberOfFilms);
 
+let personalMovieDB = {
+    count : numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
 
 
